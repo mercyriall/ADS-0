de66a673140d4afd64513b5e7823277650745224
mercyriall <73515384+mercyriall@users.noreply.github.com>
2021-03-15 20:40:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while (a != b) {
-        if (a > b)
-            a -= b;
-        else
-            b -= a;
-    }
-    return a;
+    // поместить сюда текст реализации алгоритма (на С++)
+        while ((b != 0) && (a != 0)) {
+                if (a > b)
+                        a = a - b;
+                else
+                        b = b - a;
+        }
+        return (a+b);
 }
