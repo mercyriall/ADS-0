904fa81e76f483b0d68f129404a6ccebdd554aa3
mercyriall <73515384+mercyriall@users.noreply.github.com>
2021-03-15 19:52:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while(a != 0 && b != 0){
-        if a > b{
+    while(a!=0&&b!=0){
+        if (a>b){
             a = a % b;
         }
         else{
-            b = b % a;
+            b=b%a;
         }
     }
     return(a+b)
