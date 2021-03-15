aa2b8a67857ea912c310ea557c713908e5c1b61b
mercyriall <73515384+mercyriall@users.noreply.github.com>
2021-03-15 20:16:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,4 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
-    while(a!=0&&b!=0){
-        if a > b{
-            a=a%b;
-        }
-        else{
-            b=b%a;
-        }
-    }
-    return(a+b)
 }
